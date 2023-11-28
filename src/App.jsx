17556829ec9d8ee5277e332deea68c7e965aa82e
Unclebigbay <ayodelesamueladebayo143@gmail.com>
2023-11-28 23:44:07 +0100

    remove nested routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,12 @@
 import { Route, Routes } from "react-router-dom";
 import TodoDashboard from "./pages/TodoDashboard";
 import SingleTodoPreview from "./pages/SingleTodoPreview";
-import DashboardLayout from "./pages/dashboard/DashboardLayout";
-import Profile from "./pages/dashboard/Profile";
-import Index from "./pages/dashboard/Index";
 
 function App() {
   return (
     <Routes>
       <Route path='/' element={<TodoDashboard />} />
       <Route path='/todo/:todo_id' element={<SingleTodoPreview />} />
-      <Route path='/dashboard' element={<DashboardLayout />}>
-        <Route index element={<Index />} />
-        <Route path='profile' element={<Profile />} />
-      </Route>
     </Routes>
   );
 }
